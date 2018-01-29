ac9745fb72df2ad3d6afb34aa06827b2f54a183c
cjqpker <cjq1989@gmail.com>
2018-01-29 14:57:42 +0800

    add subcmd: 'account cal'

--- src/client/commands/accounts.go
+++ src/client/commands/accounts.go
@@ -1,8 +1,10 @@
 package commands
 
 import (
+	"encoding/hex"
 	"fmt"
 
+	gcommon "github.com/Baptist-Publication/chorus-module/lib/go-common"
 	"github.com/Baptist-Publication/chorus-module/lib/go-crypto"
 	"gopkg.in/urfave/cli.v1"
 )
@@ -20,6 +22,15 @@ var (
 				Usage:    "generate new private-pub key pair",
 				Category: "Account",
 			},
+			{
+				Name:     "cal",
+				Action:   calculatePrivPubAddr,
+				Usage:    "calculate public key and address from private key",
+				Category: "Account",
+				Flags: []cli.Flag{
+					anntoolFlags.privkey,
+				},
+			},
 		},
 	}
 )
@@ -30,6 +41,28 @@ func generatePrivPubAddr(ctx *cli.Context) error {
 
 	fmt.Printf("privkey: %X\n", sk[:])
 	fmt.Printf("pubkey: %X\n", pk[:])
+
+	return nil
+}
+
+func calculatePrivPubAddr(ctx *cli.Context) error {
+	if !ctx.IsSet("privkey") {
+		return cli.NewExitError("private key is required", -1)
+	}
+
+	skBs, err := hex.DecodeString(gcommon.SanitizeHex(ctx.String("privkey")))
+	if err != nil {
+		return cli.NewExitError(err.Error(), -1)
+	}
+
+	var sk crypto.PrivKeyEd25519
+	copy(sk[:], skBs)
+
+	pk := sk.PubKey().(*crypto.PubKeyEd25519)
+	addr := pk.Address()
+
+	fmt.Printf("pubkey : %X\n", pk[:])
+	fmt.Printf("address: %X\n", addr)
 
 	return nil
 }
