ed9686b5f48281fd9fe50749fffe7efab40b79e3
sunliang <sunliang711@163.com>
2018-05-07 11:12:43 +0800

    sort BlockTx group by sender order by average gas descending

--- types/tx.go
+++ types/tx.go
@@ -10,6 +10,7 @@ import (
 	"github.com/Baptist-Publication/chorus/eth/rlp"
 	"github.com/Baptist-Publication/chorus/module/lib/ed25519"
 	gcrypto "github.com/Baptist-Publication/chorus/module/lib/go-crypto"
+	"sort"
 )
 
 type EcoInitTokenTx struct {
@@ -188,4 +189,99 @@ func (tx *BlockTx) Hash() []byte {
 	}
 
 	return crypto.Sha256(bs)
+}
+
+//sort BlockTx
+type BlockTxsToSort []BlockTx
+
+func (txs BlockTxsToSort) Len() int {
+	return len(txs)
+}
+
+func (txs BlockTxsToSort) Swap(i, j int) {
+	txs[i], txs[j] = txs[j], txs[i]
+}
+
+func (txs BlockTxsToSort) Less(i, j int) bool {
+	return txs[i].Nonce < txs[j].Nonce
+}
+
+// collectTxs generates a 'sender:BlockTxs' pair
+func collectTxs(txs []BlockTx) map[string][]BlockTx {
+	if txs == nil || len(txs) == 0 {
+		return nil
+	}
+	m := make(map[string][]BlockTx)
+	for _, tx := range txs {
+		m[string(tx.Sender)] = append(m[string(tx.Sender)], tx)
+	}
+	for sender := range m {
+		sort.Sort(BlockTxsToSort(m[sender]))
+	}
+	return m
+}
+
+// makeSenderGases generate descending senderGas
+func makeSenderGases(m map[string][]BlockTx) []senderGas {
+	if m == nil || len(m) == 0 {
+		return nil
+	}
+	var sgs []senderGas
+	for sender, txs := range m {
+		txLen := len(txs)
+		if txLen == 0 {
+			continue
+		}
+		totalGas := big.NewInt(0)
+		for _, tx := range txs {
+			totalGas.Add(totalGas, tx.GasPrice.Mul(tx.GasPrice, tx.GasLimit))
+		}
+		averageGas := totalGas.Div(totalGas, big.NewInt(int64(txLen)))
+		sg := senderGas{sender, averageGas}
+		sgs = append(sgs, sg)
+	}
+	sort.Sort(senderGasToSort(sgs))
+	return sgs
+}
+
+// SortTxs sorts txs
+func SortTxs(txs []BlockTx) []BlockTx {
+	if txs == nil || len(txs) == 0 {
+		return nil
+	}
+
+	m := collectTxs(txs)
+	if m == nil || len(m) == 0 {
+		return nil
+	}
+
+	sgs := makeSenderGases(m)
+	if sgs == nil || len(sgs) == 0 {
+		return nil
+	}
+
+	var ret []BlockTx
+	for _, sg := range sgs {
+		ret = append(ret, m[sg.sender]...)
+	}
+	return ret
+}
+
+type senderGas struct {
+	sender string
+	gas    *big.Int
+}
+
+type senderGasToSort []senderGas
+
+func (sg senderGasToSort) Len() int {
+	return len(sg)
+}
+
+func (sg senderGasToSort) Swap(i, j int) {
+	sg[i], sg[j] = sg[j], sg[i]
+}
+
+func (sg senderGasToSort) Less(i, j int) bool {
+	return sg[i].gas.Cmp(sg[j].gas) > 0
 }
